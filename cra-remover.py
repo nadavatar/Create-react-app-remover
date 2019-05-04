ccaf601761df0d0a9e4002326c3b7436e8db9923
nadavatar <nadavatar64@gmail.com>
2019-05-04 20:11:32 +0300

    The project is ready to go

--- cra-remover.py
+++ cra-remover.py
@@ -1,8 +1,5 @@
 import os
 
-# # get the current directory
-# cur_dir = os.path.dirname(os.path.abspath(__file__))
-
 # start removing the unneeded files
 os.remove('./public/favicon.ico')
 os.remove('./src/App.css')
@@ -10,11 +7,36 @@ os.remove('./src/App.test.js')
 os.remove('./src/logo.svg')
 os.remove('./src/serviceWorker.js')
 
-# start removing the unneeded lines in index.html
+# start removing the unneeded line in index.html
 with open('./public/index.html', 'r') as infile:
     lines = infile.readlines()
 
 with open('./public/index.html', 'w') as outfile:
     for pos, line in enumerate(lines):
-        if pos != 5:
+        if pos != 4:
             outfile.write(line)
+
+# start removing the unneeded lines in app.js
+with open('./src/App.js', 'r') as infile:
+    lines = infile.readlines()
+
+app_unneeded_lines = [1, 2]
+app_unneeded_lines.extend(list(range(7, 21)))
+
+with open('./src/App.js', 'w') as outfile:
+    for pos, line in enumerate(lines):
+        if pos not in app_unneeded_lines:
+            outfile.write(line)
+
+# start removing the unneeded lines in index.js
+with open('./src/index.js', 'r') as infile:
+    lines = infile.readlines()
+
+index_unneeded_lines = [4, 8, 9, 10, 11]
+
+with open('./src/index.js', 'w') as outfile:
+    for pos, line in enumerate(lines):
+        if pos not in index_unneeded_lines:
+            outfile.write(line)
+
+print('Your project is now clean and ready to go, \nEnjoy!')
